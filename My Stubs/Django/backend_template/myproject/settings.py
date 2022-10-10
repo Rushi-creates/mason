4d47b1e9b5c4a2e0daaf87774a92c77bb36ac7f1
Rushi-creates <76180114+Rushi-creates@users.noreply.github.com>
2022-10-10 05:31:38 +0530

    Update settings.py

--- My Stubs/Django/backend_template/myproject/settings.py
+++ My Stubs/Django/backend_template/myproject/settings.py
@@ -137,7 +137,12 @@ REST_FRAMEWORK = {
  }
 
 #SMTP Configuration
-
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = ''       # our sender email
+EMAIL_HOST_PASSWORD = ''       # our sender email's pass 
 
 
 # ---------------------------------------------------------------------------- #
